9dd07169eab38df931710dc8bdcdeb87cdd6d1fa
ilyichpy <ilyaszuev25@gmail.com>
2024-11-14 20:08:22 +0300

    Изменение настроек для верификации

--- src/main/java/ru/zuev/application/emailCode/controller/EmailController.java
+++ src/main/java/ru/zuev/application/emailCode/controller/EmailController.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.zuev.application.emailCode.service.EmailCodeGeneratorService;
 import ru.zuev.application.emailCode.service.EmailSenderService;
 
 import javax.mail.MessagingException;
 
 @Controller
-@RequestMapping("v1/api/email/generate_code")
+@RequestMapping("v1/api/email")
 public class EmailController {
 
 	private EmailSenderService emailSenderService;
@@ -21,7 +22,7 @@ public class EmailController {
 	}
 
 	@GetMapping("/send_code")
-	public String generateAndSendEmail(@RequestBody String userEmail) throws MessagingException {
+	public String generateAndSendEmail(@RequestParam String userEmail) throws MessagingException {
 		emailSenderService.sendMessage(userEmail);
 		return "true";
 	}
